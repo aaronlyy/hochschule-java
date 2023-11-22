d851fc2fdebc4a01ab939a9f2b8187c368131a41
Aaron Levi Can <aaronlevican@gmail.com>
2023-11-22 12:53:19 +0100

    woche4 examples

--- woche4/solutions/Methods.java
+++ woche4/solutions/Methods.java
@@ -1,7 +1,20 @@
-package woche4.solutions;
 public class Methods {
   public static void main(String[] args) {
+
+    System.out.println(greatestCommonFactor(12, 98));
+    System.out.println(greatestCommonFactor(77, 34));
+    System.out.println(greatestCommonFactor(45, 103));
+    System.out.println(greatestCommonFactor(88,77));
+
+    printReduced(55, 87);
+    printReduced(46, 88);
+    printReduced(78, 23);
+    printReduced(63, 19);
+
     add(3,2,1,6);
+    add(34,12,5,30);
+    add(45,90,20,26);
+    add(4,12,3,18);
   }
 
   // euclidian algorithm to get the greatest common factor
@@ -18,7 +31,7 @@ public class Methods {
   // divide with ggt
   public static void printReduced(int counter, int denominator) {
     int ggt = greatestCommonFactor(counter, denominator);
-    System.out.printf("%d/%d", counter / ggt, denominator / ggt);
+    System.out.printf("%d/%d\n", counter / ggt, denominator / ggt);
   }
 
   public static void add(int counter1, int denominator1, int counter2, int denominator2) {
