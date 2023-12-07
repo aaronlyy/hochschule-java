ec5ad5c38176756e58284239419f53daff47bcc4
aaronlyy <aaronlevican@gmail.com>
2023-12-07 23:27:33 +0100

    added new week5 version

--- woche5/solutions/StringHomework.java
+++ woche5/solutions/StringHomework.java
@@ -1,48 +1,62 @@
-import java.util.Arrays;
-
 public class StringHomework {
 
   public static void main(String[] args) {
-
-    if (isPalindrome("Legovogel")){
-      System.out.println("Ist ein Palindrom");
-    }
-
-    if (checkForAnagram("tom marvolo riddle", "i am lord voldemort")) {
-      System.out.println("Ist ein Anagramm");
-    }
+    System.out.println(isPalindrome("Abba"));
+    System.out.println(isPalindrome("Legovogel"));
+    System.out.println(checkForAnagrams("betruG", "Geburt"));
+    System.out.println(checkForAnagrams("tom marvolo riddle", "i am lord voldemort"));
   }
 
   public static boolean isPalindrome(String s) {
-    return s.equalsIgnoreCase(new StringBuilder(s).reverse().toString());
-  }
-
-  public static boolean checkForAnagram(String s1, String s2) {
-    s1 = s1.replaceAll("\\s+", "");
-    s2 = s2.replaceAll("\\s+", "");
-
-    if (s1.length() != s2.length()) {
+    // check if s is empty
+    if (s == "") {
       return false;
     }
 
-    // hätte man auch einfach so lösen können aber war ja nicht erlaubt :(
-    // char[] charArray1 = s1.toCharArray();
-    // char[] charArray2 = s2.toCharArray();
+    // remove spaces and ignore case
+    s = s.replaceAll("\\s", "").toLowerCase();
 
-    char[] s1Arr = new char[s1.length()];
-    char[] s2Arr = new char[s2.length()];
+    // loop throug string till half of it from both sides and compare chars
+    for (int i = 0; i < s.length() / 2; i++) {
+      if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+        return false;
+      }
+    }
 
+    return true;
+  }
+
+  public static boolean checkForAnagrams(String s1, String s2) {
+    // check if strings are empty
+    if (s1 == "" || s2 == "") {
+        return false;
+    }
+
+    // removed spaces
+    s1 = s1.replaceAll("\\s+", "");
+    s2 = s2.replaceAll("\\s+", "");
+
+    // check if strings are same length
+    if (s1.length() != s2.length()) {
+        return false;
+    }
+
+    // create char array (ASCII)
+    int[] charCounts = new int[256];
+
+    // count chars
     for (int i = 0; i < s1.length(); i++) {
-      s1Arr[i] = s1.charAt(i);
+        charCounts[s1.charAt(i)]++;
+        charCounts[s2.charAt(i)]--;
     }
 
-    for (int i = 0; i < s2.length(); i++) {
-      s2Arr[i] = s2.charAt(i);
+    // check if every char is 0
+    for (int count : charCounts) {
+      if (count != 0) {
+        return false;
+      }
     }
 
-    Arrays.sort(s1Arr);
-    Arrays.sort(s2Arr);
-
-    return Arrays.equals(s1Arr, s2Arr);
+    return true;
   }
 }
